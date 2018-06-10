cb52b7eee8ae1d2d6211b6c8b6e5e65ba92b9dd0
Alex RUDENKO <arudenko@e1r9p11.unit.ua>
2018-06-10 20:07:23 +0300

    VenyaGryaz

--- src/gui/rotate_cam.c
+++ src/gui/rotate_cam.c
@@ -17,45 +17,55 @@ void	camera_move(t_view *s, SDL_Scancode key)
 	if (key == SDL_SCANCODE_W)
 	{
 		s->space->cam->o.y += 0.5;
-		redraw(s);
 	}
 	else if (key == SDL_SCANCODE_S)
 	{
 		s->space->cam->o.y -= 0.5;
-		redraw(s);
-	}
-	else if (key == SDL_SCANCODE_D)
-	{
-		s->space->cam->o.x -= 0.5;
-		redraw(s);
 	}
 	else if (key == SDL_SCANCODE_A)
 	{
-		s->space->cam->o.x += 0.5;
-		redraw(s);
+		s->space->cam->o.x -= 0.5;
 	}
+	else if (key == SDL_SCANCODE_D)
+	{
+		s->space->cam->o.x += 0.5;
+	}
+	else if (key == SDL_SCANCODE_Z)
+	{
+		s->space->cam->o.z -= 0.5;
+	}
+	else if (key == SDL_SCANCODE_C)
+	{
+		s->space->cam->o.z += 0.5;
+	}
+	redraw(s);
 }
 
 void	camera_rot(t_view *s, SDL_Scancode key)
 {
-	if (key == SDL_SCANCODE_UP)
-	{
-		s->space->cam->v.y += 0.5;
-		redraw(s);
-	}
-	else if (key == SDL_SCANCODE_DOWN)
-	{
-		s->space->cam->v.y -= 0.5;
-		redraw(s);
-	}
-	else if (key == SDL_SCANCODE_RIGHT)
+	if (key == SDL_SCANCODE_KP_8)
 	{
 		s->space->cam->v.x += 0.5;
-		redraw(s);
 	}
-	else if (key == SDL_SCANCODE_LEFT)
+	else if (key == SDL_SCANCODE_KP_2)
 	{
 		s->space->cam->v.x -= 0.5;
-		redraw(s);
 	}
+	else if (key == SDL_SCANCODE_KP_4)
+	{
+		s->space->cam->v.y -= 0.5;
+	}
+	else if (key == SDL_SCANCODE_KP_6)
+	{
+		s->space->cam->v.y += 0.5;
+	}
+	else if (key == SDL_SCANCODE_KP_7)
+	{
+		s->space->cam->v.z += 0.5;
+	}
+	else if (key == SDL_SCANCODE_KP_3)
+	{
+		s->space->cam->v.z -= 0.5;
+	}
+	redraw(s);
 }
