9f633f7931c0aeab8d6bfe408094afc20fecb7e0
Veniamin VINOGRADOV <vvinogra@e1r9p12.unit.ua>
2018-06-11 16:34:46 +0300

    fixed parse refraction

--- src/parse/preflection.c
+++ src/parse/preflection.c
@@ -6,13 +6,13 @@
 /*   By: vvinogra <vvinogra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/08 12:46:44 by knovytsk          #+#    #+#             */
-/*   Updated: 2018/06/10 20:47:53 by vvinogra         ###   ########.fr       */
+/*   Updated: 2018/06/11 16:31:50 by vvinogra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-#define NO_REFRACTION 101
+#define NO_REFRACTION -1
 
 void		parse_color_reflection(JSON_Object *fobject, t_figure *figure)
 {
@@ -30,10 +30,10 @@ void		parse_color_reflection(JSON_Object *fobject, t_figure *figure)
 	if (json_object_has_value_of_type(fobject, "refraction", JSONNumber))
 	{
 		figure->refract = json_object_get_number(fobject, "refraction");
-		if (figure->refract < -100)
-			figure->refract = -100;
-		if (figure->refract > 100)
-			figure->refract = 100;
+		if (figure->refract < 0)
+			figure->refract = -1;
+		if (figure->refract > 200)
+			figure->refract = 200;
 	}
 	else
 		figure->refract = NO_REFRACTION;
